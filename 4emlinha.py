1a062204fe2baaa2412749df4d875559192381d8
PedroAntunes178 <pedronmantunes@gmail.com>
2019-11-21 15:13:09 +0000

    Update 4emlinha.py

--- 4emlinha.py
+++ 4emlinha.py
@@ -3,5 +3,13 @@ import numpy as np
 NUM_COLUNAS = 8
 NUM_LINHAS = 10
 
-def criar_board():
-    
+def criar_tabuleiro():
+    tabuleiro = np.zeros((NUM_LINHAS,NUM_COLUNAS))
+    return tabuleiro
+
+def imprimir_tabuleiro(tabuleiro):
+	print(np.flip(tabuleiro, 0))
+
+tabuleiro = criar_tabuleiro()
+game_over = False
+jogador = 0
