d6ccdd53d11af64a69cb3cf547694caf68aac9c7
mariana <mariana.fidalgo.f>
2019-11-23 18:22:45 +0000

    comments and identations

--- 4emlinha.py
+++ 4emlinha.py
@@ -1,4 +1,4 @@
-#importes
+#imports
 import numpy as np
 import pygame
 import sys
@@ -10,32 +10,33 @@ NUM_COLUNAS = 6
 NUM_LINHAS = 7
 MEDIDA_POR_QUADRADO = 100 #isto está em pixeis
 RAIO_PECA = int(MEDIDA_POR_QUADRADO/2 - 5)
+
 #cores
 AZUL_NEEC = (0, 157, 224)
 PRETO = (0, 0, 0)
 
-#esta função está a criar uma matriz de zeros, isto para simular o nosso tabuleiro com x colunas e y linhas
+#esta função cria uma matriz de zeros, neste caso é o nosso tabuleiro com x colunas e y linhas
 def criar_tabuleiro():
-    tabuleiro = np.zeros((NUM_LINHAS,NUM_COLUNAS))
-    return tabuleiro
+	tabuleiro = np.zeros((NUM_LINHAS,NUM_COLUNAS))
+	return tabuleiro
 
-#esta função serve para imprimir o tabuleiro/matriz no terminar da forma como é suposto a vermos (ou seja com o [0][0] no canto inferior esquerdo, se fizesse só print este estaria no canto superior)
+#esta função serve para imprimir o tabuleiro/matriz no terminal (com o [0][0] no canto inferior esquerdo, se fizesse só print este estaria no canto superior)????
 def imprimir_tabuleiro(tabuleiro):
 	print(np.flip(tabuleiro, 0))
 
 #esta função verifica se a coluna selecionada tem espaço para colocar a peça
 def coluna_cheia(tabuleiro):
-    pass
+	pass
 
 #esta função vê qual é a casa da matriz em que a peça vai ficar e posiciona a peça no tabuleiro
 def posicao_peca(tabuleiro):
-    pass
+	pass
 
-#está função retorna True se for feito 4 em linha
+#esta função retorna True se for feito 4 em linha
 def vitoria(tabuleiro):
-    pass
+	pass
 
-#está funão desenha o tabuleiro em pygame
+#esta função desenha o tabuleiro em pygame
 def desenhar_tabuleiro(tabuleiro):
 	for c in range(NUM_COLUNAS):
 		for r in range(NUM_LINHAS):
@@ -50,34 +51,34 @@ def desenhar_tabuleiro(tabuleiro):
 				pygame.draw.circle(screen, PECA2, (int(c*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2), height-int(r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2)), RAIO_PECA)
 	pygame.display.update()
 
-#está função é onde vai correr o jogo
+#esta função main que corre o jogo
 def main():
-    tabuleiro = criar_tabuleiro()
-    game_over = False
-    vez_de = 0
+	tabuleiro = criar_tabuleiro()
+	game_over = False
+	vez_de = 0
 
-    pygame.init()
-    width = NUM_COLUNAS * MEDIDA_POR_QUADRADO
-    height = (NUM_LINHAS+1) * MEDIDA_POR_QUADRADO
-    size = (width, height)
-    screen = pygame.display.set_mode(size)
-    desenhar_tabuleiro(tabuleiro)
-    pygame.display.update()
-    myfont = pygame.font.SysFont("monospace", 75)
+	pygame.init()
+	width = NUM_COLUNAS * MEDIDA_POR_QUADRADO
+	height = (NUM_LINHAS+1) * MEDIDA_POR_QUADRADO
+	size = (width, height)
+	screen = pygame.display.set_mode(size)
+	desenhar_tabuleiro(tabuleiro)
+	pygame.display.update()
+	myfont = pygame.font.SysFont("monospace", 75)
 
-    while not(game_over):
-        for event in pygame.event.get():
-    		if event.type == pygame.QUIT:
-    			sys.exit(0)
-
-            if event.type == pygame.MOUSEBUTTONDOWN:
+	while not(game_over):
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit(0)
+			
+			if event.type == pygame.MOUSEBUTTONDOWN:
 				posx = event.pos[0]
 				col = int(math.floor(posx/SQUARESIZE))
-                if vez_de == 0:
-                    pass
-                else:
-                    pass
-                vez_de += 1
-                vez_de = vez_de%2
+				if vez_de == 0:
+					pass
+				else:
+					pass
+				vez_de += 1
+				vez_de = vez_de%2
 
 main()
