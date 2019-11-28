c1308274c4b963d471e7a50ca8e02da5bce066a9
PedroAntunes178 <pedronmantunes@gmail.com>
2019-11-28 16:23:30 +0000

    Update 4emlinha.py

--- 4emlinha.py
+++ 4emlinha.py
@@ -14,6 +14,7 @@ RAIO_PECA = int(MEDIDA_POR_QUADRADO/2 - 5)
 # definição de cores
 AZUL_NEEC = (0, 157, 224)
 PRETO = (0, 0, 0)
+BRANCO = (225, 225, 225)
 PECA1 = (225, 0, 0)
 PECA2 = (0, 225, 225)
 
@@ -34,10 +35,11 @@ def imprimir_tabuleiro(tabuleiro):
 #        return True
 
 #esta função vê qual é a casa da matriz em que a peça vai ficar e posiciona a peça no tabuleiro
-def colocar_peca_no_tabuleiro(tabuleiro, col, player):
+def colocar_peca_no_tabuleiro(tabuleiro, col, player, lin):
     for linha in range(NUM_LINHAS):
         if tabuleiro[linha][col] == 0:
             tabuleiro[linha][col] = player
+            lin = linha
             return True
     return False
 
@@ -67,7 +69,7 @@ def vitoria(tabuleiro, peca):
     return False
 
 #esta funão desenha o tabuleiro em pygame
-def desenhar_tabuleiro(tabuleiro, screen, height):
+def desenhar_tabuleiro(tabuleiro, screen, altura):
 	for c in range(NUM_COLUNAS):
 		for r in range(NUM_LINHAS):
 			pygame.draw.rect(screen, AZUL_NEEC, (c*MEDIDA_POR_QUADRADO, r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO, MEDIDA_POR_QUADRADO, MEDIDA_POR_QUADRADO))
@@ -76,9 +78,9 @@ def desenhar_tabuleiro(tabuleiro, screen, height):
 	for c in range(NUM_COLUNAS):
 		for r in range(NUM_LINHAS):
 			if tabuleiro[r][c] == 1:
-				pygame.draw.circle(screen, PECA1, (int(c*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2), height-int(r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2)), RAIO_PECA)
+				pygame.draw.circle(screen, PECA1, (int(c*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2), altura-int(r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2)), RAIO_PECA)
 			elif tabuleiro[r][c] == 2:
-				pygame.draw.circle(screen, PECA2, (int(c*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2), height-int(r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2)), RAIO_PECA)
+				pygame.draw.circle(screen, PECA2, (int(c*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2), altura-int(r*MEDIDA_POR_QUADRADO+MEDIDA_POR_QUADRADO/2)), RAIO_PECA)
 	pygame.display.update()
 
 #esta função é onde vai correr o jogo
@@ -88,11 +90,11 @@ def main():
     vez_de = 0
 
     pygame.init()
-    width = NUM_COLUNAS * MEDIDA_POR_QUADRADO
-    height = (NUM_LINHAS+1) * MEDIDA_POR_QUADRADO
-    size = (width, height)
-    screen = pygame.display.set_mode(size)
-    desenhar_tabuleiro(tabuleiro, screen, height)
+    largura = NUM_COLUNAS * MEDIDA_POR_QUADRADO
+    altura = (NUM_LINHAS+1) * MEDIDA_POR_QUADRADO
+    tamanho = (largura, altura)
+    screen = pygame.display.set_mode(tamanho)
+    desenhar_tabuleiro(tabuleiro, screen, altura)
     pygame.display.update()
     myfont = pygame.font.SysFont("monospace", 75)
 
@@ -103,15 +105,16 @@ def main():
             if event.type == pygame.MOUSEBUTTONDOWN :
                 posx = event.pos[0]
                 col = int(math.floor(posx/MEDIDA_POR_QUADRADO))
+                lin = -1
                 if vez_de == 0:
-                    if not(colocar_peca_no_tabuleiro(tabuleiro, col, 1)):
+                    if not(colocar_peca_no_tabuleiro(tabuleiro, col, 1, lin)):
                         vez_de += 1
                     if vitoria(tabuleiro, 1):
                         label = myfont.render("Player 1 wins!!", 1, PECA1)
                         screen.blit(label, (40,10))
                         game_over = True
                 else:
-                    if not(colocar_peca_no_tabuleiro(tabuleiro, col, 2)):
+                    if not(colocar_peca_no_tabuleiro(tabuleiro, col, 2, lin)):
                         vez_de += 1
                     if vitoria(tabuleiro, 2):
                         label = myfont.render("Player 2 wins!!", 1, PECA2)
@@ -119,9 +122,13 @@ def main():
                         game_over = True
                 vez_de += 1
                 vez_de = vez_de%2
-                desenhar_tabuleiro(tabuleiro, screen, height)
+                desenhar_tabuleiro(tabuleiro, screen, altura)
                 imprimir_tabuleiro(tabuleiro)
             if game_over:
-                time.sleep(3.00)
+                pygame.draw.rect(screen, PRETO, (int((largura-MEDIDA_POR_QUADRADO)/2), int((altura-MEDIDA_POR_QUADRADO)/2) , MEDIDA_POR_QUADRADO, MEDIDA_POR_QUADRADO))
+                for k in range(3):
+                    time_num = myfont.render(str(k), 1, PECA2)
+                    screen.blit(time_num, (int((largura-MEDIDA_POR_QUADRADO)/2),int((altura-MEDIDA_POR_QUADRADO)/2)))
+                    time.sleep(1.00)
 
 main()
