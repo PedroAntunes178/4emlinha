5c60e0086e0ea2a2b7b9f5de2cf14c8b92df5134
PedroAntunes178 <pedronmantunes@gmail.com>
2019-12-01 22:04:41 +0000

    Update 4emlinha.py

--- 4emlinha.py
+++ 4emlinha.py
@@ -186,14 +186,14 @@ def main():
                 #imprimir_tabuleiro(tabuleiro)
 
             if game_over:
+                pygame.draw.rect(screen, BRANCO, (int((largura-5*MEDIDA_POR_QUADRADO)/2), int((altura-MEDIDA_POR_QUADRADO)/2) , 5*MEDIDA_POR_QUADRADO, 2*MEDIDA_POR_QUADRADO))
+                pygame.draw.rect(screen, PRETO, (int((largura-5*MEDIDA_POR_QUADRADO)/2), int((altura-MEDIDA_POR_QUADRADO)/2), 5*MEDIDA_POR_QUADRADO, MEDIDA_POR_QUADRADO))
+                pygame.draw.rect(screen, BRANCO, (int((largura-5*MEDIDA_POR_QUADRADO)/2)+4, int((altura-MEDIDA_POR_QUADRADO)/2)+4, 5*MEDIDA_POR_QUADRADO-8, MEDIDA_POR_QUADRADO-8))
+                label = myfont.render("Restart!", 1, PRETO)
+                screen.blit(label, (int((largura-3*MEDIDA_POR_QUADRADO)/2)+2, int((largura-3*MEDIDA_POR_QUADRADO)/2+MEDIDA_POR_QUADRADO)+2))
                 for k in range(3):
-                    pygame.draw.rect(screen, BRANCO, (int((largura-5*MEDIDA_POR_QUADRADO)/2), int((altura-MEDIDA_POR_QUADRADO)/2) , 5*MEDIDA_POR_QUADRADO, 2*MEDIDA_POR_QUADRADO))
                     time_num = myfont.render(str(3-k), 1, PRETO)
-                    screen.blit(time_num, (int(largura/2),int(altura/2)))
-                    pygame.draw.rect(screen, PRETO, (int((largura-5*MEDIDA_POR_QUADRADO)/2), int((altura-MEDIDA_POR_QUADRADO)/2), 5*MEDIDA_POR_QUADRADO, int(MEDIDA_POR_QUADRADO)))
-                    pygame.draw.rect(screen, BRANCO, (int((largura-5*MEDIDA_POR_QUADRADO)/2)+4, int((altura-MEDIDA_POR_QUADRADO)/2)+4, 5*MEDIDA_POR_QUADRADO-8, int(MEDIDA_POR_QUADRADO)-8))
-                    label = myfont.render("Restart!", 1, PRETO)
-                    screen.blit(label, (int((largura-3*MEDIDA_POR_QUADRADO)/2)+2, int((largura-3*MEDIDA_POR_QUADRADO)/2+MEDIDA_POR_QUADRADO)+2))
+                    screen.blit(time_num, (int(largura/2),int((altura+2*MEDIDA_POR_QUADRADO)/2)))
                     for event in pygame.event.get() :
                         if event.type == pygame.MOUSEBUTTONDOWN :
                             pos = pygame.mouse.get_pos()
